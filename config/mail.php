2285f38484b1a5fbf50dfad7acc35636eff0e3a4
Rahul Pol <polrahul10@gmail.com>
2016-11-21 10:30:38 +0530

    Mail Configuration provided for enabling mail feature.

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'polrahul10@gmail.com', 'name' => null],
 
 	/*
 	|--------------------------------------------------------------------------
@@ -67,7 +67,7 @@ return [
 	|
 	*/
 
-	'encryption' => 'tls',
+	'encryption' => env('MAIL_ENCRYPTION', 'tls'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return [
 	|
 	*/
 
-	'password' => env('MAIL_PASSWORD'),
+	'password' => 'l0g1t3ch' ,//env('MAIL_PASSWORD'),	
 
 	/*
 	|--------------------------------------------------------------------------
