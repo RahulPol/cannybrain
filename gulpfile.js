02446543e3835d84a2a40f378c681ff9a244497f
Rahul Pol <polrahul10@gmail.com>
2016-11-21 11:12:34 +0530

    resources needed configured.

--- gulpfile.js
+++ gulpfile.js
@@ -17,84 +17,123 @@ elixir(function(mix) {
     mix
 
         .copy(
-            'node_modules/admin-lte/bootstrap/fonts',
-            'public/build/fonts'
+        'node_modules/admin-lte/bootstrap/fonts',
+        'public/build/fonts'
         )
 
         .copy(
-            'node_modules/admin-lte/dist/img',
-            'public/build/img/adminlte'
+        'node_modules/font-awesome/fonts',
+        'public/build/fonts'
         )
 
         .copy(
-            'node_modules/admin-lte/bootstrap/css',
-            'resources/assets/css/bootstrap'
+        'node_modules/ionicons/dist/fonts',
+        'public/build/fonts'
         )
 
         .copy(
-            'node_modules/admin-lte/dist/css',
-            'resources/assets/css/adminlte'
+        'node_modules/admin-lte/dist/img',
+        'public/build/img/adminlte'
         )
 
         .copy(
-            'node_modules/parsleyjs/src/parsley.css',
-            'resources/assets/css/parsley'
+        'node_modules/font-awesome/css/font-awesome.min.css',
+        'resources/assets/css/font-awesome'
         )
 
         .copy(
-            'node_modules/jquery/dist/jquery.min.js',
-            'resources/assets/js/jquery'
-        )
-//parsleyjs
-        .copy(
-            'node_modules/admin-lte/bootstrap/js/bootstrap.min.js',
-            'resources/assets/js/bootstrap'
+        'node_modules/ionicons/dist/css/ionicons.min.css',
+        'resources/assets/css/ionicons'
         )
 
         .copy(
-            'node_modules/admin-lte/dist/js',
-            'resources/assets/js/adminlte'
+        'node_modules/admin-lte/bootstrap/css',
+        'resources/assets/css/bootstrap'
         )
 
         .copy(
-            'node_modules/parsleyjs/dist/parsley.min.js',
-            'resources/assets/js/parsley'
+        'node_modules/admin-lte/dist/css',
+        'resources/assets/css/adminlte'
+        )
+
+        .copy(
+        'node_modules/admin-lte/plugins/select2/select2.min.css',
+        'resources/assets/css/select2'
+        )
+
+        .copy(
+        'node_modules/parsleyjs/src/parsley.css',
+        'resources/assets/css/parsley'
+        )
+
+        .copy(
+        'node_modules/jquery/dist/jquery.min.js',
+        'resources/assets/js/jquery'
+        )
+
+        .copy(
+        'node_modules/admin-lte/bootstrap/js/bootstrap.min.js',
+        'resources/assets/js/bootstrap'
+        )
+
+        .copy(
+        'node_modules/admin-lte/dist/js',
+        'resources/assets/js/adminlte'
+        )
+
+        .copy(
+        'node_modules/admin-lte/plugins/select2/select2.min.js',
+        'resources/assets/js/select2'
+        )
+
+        .copy(
+        'node_modules/parsleyjs/dist/parsley.min.js',
+        'resources/assets/js/parsley'
         )
 
         .styles([
+            'font-awesome/font-awesome.min.css',
+            'ionicons/ionicons.min.css',
             'bootstrap/bootstrap.min.css',
             'adminlte/skins/_all-skins.min.css',
             'adminlte/AdminLTE.min.css'
-        ],'public/css/app.css')
+        ], 'public/css/app.css')
 
         .styles([
-            'parsley/parsley.css'            
-        ],'public/css/utility.css')
+            'parsley/parsley.css',
+            'select2/select2.min.css',
+            'pace/pace-theme-flash.css',
+        ], 'public/css/utility.css')
+
+
 
         .scripts([
             'jquery/jquery.min.js',
             'bootstrap/bootstrap.min.js',
             'adminlte/app.min.js'
-        ],'public/js/app.js')
+        ], 'public/js/app.js')
 
 
-        .scripts([            
-            'parsley/parsley.min.js',            
-        ],'public/js/utility.js')
+
+        .scripts([
+            'parsley/parsley.min.js',
+            'select2/select2.min.js',
+            'pace/pace.min.js'
+        ], 'public/js/utility.js')
 
         /**
         * Apply version control
         */
 
         .version([
-         "public/css/app.css",
-         "public/js/app.js",
-         "public/css/utility.css",
-         "public/js/utility.js"         
-         ]);
-
-        
+            "public/css/app.css",
+            "public/js/app.js",
+            "public/css/utility.css",
+            "public/js/utility.js"
+        ]);
 
 
-    mix.phpUnit([] , path.normalize('vendor/bin/phpunit') + ' --verbose');
+
+
+    //mix.phpUnit([] , path.normalize('vendor/bin/phpunit') + ' --verbose');
 });
