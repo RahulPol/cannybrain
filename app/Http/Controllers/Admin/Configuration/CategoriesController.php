e247e8ab5c153120c434e2d9ccf4ae6d7bbf5dbb
Rahul <polrahul10@gmail.com>
2016-11-29 09:33:15 +0530

    Removed sleep method that simulated ajax delay.

--- app/Http/Controllers/Admin/Configuration/CategoriesController.php
+++ app/Http/Controllers/Admin/Configuration/CategoriesController.php
@@ -8,110 +8,105 @@ use Request;
 use Response;
 use Auth;
 
-class CategoriesController extends Controller {
+class CategoriesController extends Controller
+{
+    private $category;
 
-	private $category;
+    public function __construct(CategoryRepository $category)
+    {
+        $this->category = $category;
+    }
 
-	public function __construct(CategoryRepository $category)
-	{
-		$this->category = $category; 
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return view('admin.partials.configuration.categories');
+    }
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		return view('admin.partials.configuration.categories');
-	}
+    public function getAllCategories()
+    {
+        if (Request::ajax()) {
+            return $this->category->getAll();
+        }
+    }
 
-	public function getAllCategories()
-	{
-		if(Request::ajax())
-		{
-			return $this->category->getAll();
-		}
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        if (Request::ajax()) {
+            if (Auth::check() && Auth::user()->role->name == "admin") {
+                $attributes = array();
+                $attributes['name'] = Request::input('categoryName');
+                $attributes['user_id'] = Auth::user()->id;
+                $attributes['is_active'] = true;
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		if(Request::ajax()) 
-		{
-			sleep(5);
-			if(Auth::check() && Auth::user()->role->name == "user")
-			{
-				$attributes = array();
-				$attributes['name'] = Request::input('categoryName');	
-				$attributes['user_id'] = Auth::user()->id;
-				$attributes['is_active'] = true;
+                return $this->category->create($attributes);
+            }
 
-				return $this->category->create($attributes);
-			}
+                    
+            return Response::json(array('code'=>401, 'message'=> "Not authorized to create category"), 401);
+        }
+    }
 
-					
-			return Response::json(array('code'=>401, 'message'=> "Not authorized to create category"),401);
-		}
-	}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        //
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
-
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
