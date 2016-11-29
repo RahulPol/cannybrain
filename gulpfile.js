fe765cd071edfda4dd28a137d56751be8b42c307
Rahul <polrahul10@gmail.com>
2016-11-29 09:30:49 +0530

    Added moment.js, datatables.js

--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,11 @@ elixir(function(mix) {
         )
 
         .copy(
+        'node_modules/dataTables/media/images',
+        'public/build/images'
+        )
+
+        .copy(
         'node_modules/font-awesome/css/font-awesome.min.css',
         'resources/assets/css/font-awesome'
         )
@@ -67,6 +72,11 @@ elixir(function(mix) {
         )
 
         .copy(
+        'node_modules/admin-lte/plugins/datatables/dataTables.bootstrap.css',
+        'resources/assets/css/dataTables'
+        )
+
+        .copy(
         'node_modules/jquery/dist/jquery.min.js',
         'resources/assets/js/jquery'
         )
@@ -91,6 +101,26 @@ elixir(function(mix) {
         'resources/assets/js/parsley'
         )
 
+        .copy(
+        'node_modules/admin-lte/plugins/datatables/jquery.dataTables.min.js',
+        'resources/assets/js/dataTables'
+        )
+
+        .copy(
+        'node_modules/admin-lte/plugins/datatables/dataTables.bootstrap.min.js',
+        'resources/assets/js/dataTables'
+        )
+
+        .copy(
+        'node_modules/moment/min/moment.min.js',
+        'resources/assets/js/moment'
+        )
+
+        .copy(
+        'node_modules/moment/min/locales.min.js',
+        'resources/assets/js/moment'
+        )
+
         .styles([
             'font-awesome/font-awesome.min.css',
             'ionicons/ionicons.min.css',
@@ -103,6 +133,7 @@ elixir(function(mix) {
             'parsley/parsley.css',
             'select2/select2.min.css',
             'pace/pace-theme-flash.css',
+            'datatables/dataTables.bootstrap.css',
         ], 'public/css/utility.css')
 
 
@@ -118,7 +149,11 @@ elixir(function(mix) {
         .scripts([
             'parsley/parsley.min.js',
             'select2/select2.min.js',
-            'pace/pace.min.js'
+            'pace/pace.min.js',
+            'dataTables/jquery.datatables.min.js',
+            'dataTables/dataTables.bootstrap.min.js',
+            'moment/moment.min.js',
+            'moment/locales.min.js'
         ], 'public/js/utility.js')
 
         /**
