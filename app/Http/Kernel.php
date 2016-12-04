0274d2da83fddf4a4cbae51f82913a7dab38c28c
Rahul <polrahul10@gmail.com>
2016-12-05 00:14:35 +0530

    Added user authorization for all pages.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,6 +28,7 @@ class Kernel extends HttpKernel {
 		'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
 		'guest' => 'App\Http\Middleware\RedirectIfAuthenticated',
 		'auth.admin' => 'App\Http\Middleware\AuthenticateAdmin',
+		'auth.user' => 'App\Http\Middleware\AuthenticateUser',
 	];
 
 }
---
+++ app/Http/Middleware/AuthenticateUser.php
@@ -0,0 +1,62 @@
+<?php namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+
+class AuthenticateUser {
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+
+	/**
+	 * The Guard implementation.
+	 *
+	 * @var Guard
+	 */
+	protected $auth;
+
+	/**
+	 * Create a new filter instance.
+	 *
+	 * @param  Guard  $auth
+	 * @return void
+	 */
+	public function __construct(Guard $auth)
+	{
+		$this->auth = $auth;
+	}
+
+	public function handle($request, Closure $next)
+	{
+		if ($this->auth->guest())
+		{
+			if ($request->ajax())
+			{
+				return response('Unauthorized.', 401);
+			}
+			else
+			{
+				return redirect()->guest('auth/login');
+			}
+		}
+		else
+		{
+			if($this->auth->user()->role->name=='user')
+			{
+				return $next($request);
+			}
+
+			else if($this->auth->user()->role->name=='admin')
+			{
+				return redirect()->guest('/a/dashboard');				
+			}
+
+		}
+	}
+
+}
