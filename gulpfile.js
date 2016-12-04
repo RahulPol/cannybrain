a1cf0687d102295edf0db37f225cb15336c78d8f
Rahul <polrahul10@gmail.com>
2016-12-05 00:12:50 +0530

    Added Jquery Confirm  and segregated datatables

--- gulpfile.js
+++ gulpfile.js
@@ -12,160 +12,184 @@ var path = require('path');
  |
  */
 
-elixir(function(mix) {
+elixir(function (mix) {
     //mix.less('app.less');
     mix
 
         .copy(
         'node_modules/admin-lte/bootstrap/fonts',
         'public/build/fonts'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/font-awesome/fonts',
         'public/build/fonts'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/ionicons/dist/fonts',
         'public/build/fonts'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/admin-lte/dist/img',
         'public/build/img/adminlte'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/dataTables/media/images',
         'public/build/images'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/font-awesome/css/font-awesome.min.css',
         'resources/assets/css/font-awesome'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/ionicons/dist/css/ionicons.min.css',
         'resources/assets/css/ionicons'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/admin-lte/bootstrap/css',
         'resources/assets/css/bootstrap'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/admin-lte/dist/css',
         'resources/assets/css/adminlte'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/admin-lte/plugins/select2/select2.min.css',
         'resources/assets/css/select2'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/parsleyjs/src/parsley.css',
         'resources/assets/css/parsley'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/admin-lte/plugins/datatables/dataTables.bootstrap.css',
         'resources/assets/css/dataTables'
-        )
+    )
 
-        .copy(
+    .copy(
+        'node_modules/jquery-confirm/dist/jquery-confirm.min.css',
+        'resources/assets/css/jquery-confirm/jquery.confirm.min.css'
+    )
+
+    .copy(
         'node_modules/jquery/dist/jquery.min.js',
         'resources/assets/js/jquery'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/admin-lte/bootstrap/js/bootstrap.min.js',
         'resources/assets/js/bootstrap'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/admin-lte/dist/js',
         'resources/assets/js/adminlte'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/admin-lte/plugins/select2/select2.min.js',
         'resources/assets/js/select2'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/parsleyjs/dist/parsley.min.js',
         'resources/assets/js/parsley'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/admin-lte/plugins/datatables/jquery.dataTables.min.js',
         'resources/assets/js/dataTables'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/admin-lte/plugins/datatables/dataTables.bootstrap.min.js',
         'resources/assets/js/dataTables'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/moment/min/moment.min.js',
         'resources/assets/js/moment'
-        )
+    )
 
-        .copy(
+    .copy(
         'node_modules/moment/min/locales.min.js',
         'resources/assets/js/moment'
-        )
+    )
 
-        .styles([
-            'font-awesome/font-awesome.min.css',
-            'ionicons/ionicons.min.css',
-            'bootstrap/bootstrap.min.css',
-            'adminlte/skins/_all-skins.min.css',
-            'adminlte/AdminLTE.min.css'
-        ], 'public/css/app.css')
+    .copy(
+        'node_modules/jquery-confirm/dist/jquery-confirm.min.js',
+        'resources/assets/js/jquery-confirm/jquery.confirm.min.js'
+    )
 
-        .styles([
-            'parsley/parsley.css',
-            'select2/select2.min.css',
-            'pace/pace-theme-flash.css',
-            'datatables/dataTables.bootstrap.css',
-        ], 'public/css/utility.css')
+    .styles([
+        'font-awesome/font-awesome.min.css',
+        'ionicons/ionicons.min.css',
+        'bootstrap/bootstrap.min.css',
+        'adminlte/skins/_all-skins.min.css',
+        'adminlte/AdminLTE.min.css'
+    ], 'public/css/app.css')
+
+    .styles([
+        'parsley/parsley.css',
+        'select2/select2.min.css',
+        'pace/pace-theme-flash.css',
+        'jquery-confirm/jquery.confirm.min.css',
+        'cannybrain/site.css'
+    ], 'public/css/utility.css')
+
+    .styles([
+        'datatables/dataTables.bootstrap.css',
+        'datatables/buttons.dataTables.min.css',
+        'datatables/select.dataTables.min.css'
+    ], 'public/css/dataTables.css')
 
 
-
-        .scripts([
-            'jquery/jquery.min.js',
-            'bootstrap/bootstrap.min.js',
-            'adminlte/app.min.js'
-        ], 'public/js/app.js')
-
+    .scripts([
+        'jquery/jquery.min.js',
+        'bootstrap/bootstrap.min.js',
+        'adminlte/app.min.js'
+    ], 'public/js/app.js')
 
 
-        .scripts([
-            'parsley/parsley.min.js',
-            'select2/select2.min.js',
-            'pace/pace.min.js',
-            'dataTables/jquery.datatables.min.js',
-            'dataTables/dataTables.bootstrap.min.js',
-            'moment/moment.min.js',
-            'moment/locales.min.js'
-        ], 'public/js/utility.js')
+    .scripts([
+        'parsley/parsley.min.js',
+        'select2/select2.min.js',
+        'pace/pace.min.js',
+        'moment/moment.min.js',
+        'moment/locales.min.js',
+        'jquery-confirm/jquery.confirm.min.js',
+        'cannybrain/site.js'
+    ], 'public/js/utility.js')
 
-        /**
-        * Apply version control
-        */
+    .scripts([
+        'dataTables/jquery.datatables.min.js',
+        'dataTables/dataTables.bootstrap.min.js',
+        'dataTables/dataTables.buttons.min.js',
+        'dataTables/dataTables.select.min.js'
+    ], 'public/js/dataTables.js')
 
-        .version([
-            "public/css/app.css",
-            "public/js/app.js",
-            "public/css/utility.css",
-            "public/js/utility.js"
-        ]);
+    /**
+     * Apply version control
+     */
+
+    .version([
+        "public/css/app.css",
+        "public/js/app.js",
+        "public/css/utility.css",
+        "public/js/utility.js",
+        "public/css/dataTables.css",
+        "public/js/dataTables.js"
+    ]);
 
 
 
